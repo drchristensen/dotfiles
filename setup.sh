de3647e98f1a29a768a087128fc16c25f7dead0e
David Christensen <drc@ibm.com>
2021-02-25 13:56:28 -0800

    Updated setup script, move old init files if they exist before replacing

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ while getopts "vib" args; do
 done
 
 # Store where the script was called from so we can reference it later
-script_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update bash-it if it's already installed or download it if it's not
 echo "Installing/updating bash-it..."
@@ -29,31 +29,14 @@ else
 fi
 
 # Add our custom aliases to bash-it
-ln $params $script_home/custom.aliases.bash $HOME/.bash_it/aliases/custom.aliases.bash
+ln $params $SCRIPT_HOME/custom.aliases.bash $HOME/.bash_it/aliases/custom.aliases.bash
 
 # Add our custom libraries to bash-it
-ln $params $script_home/custom.bash $HOME/.bash_it/lib/custom.bash
-
-# Add solarized colors for vim if not present
-if [ ! -f $HOME/.vim/colors/solarized.vim ]; then
-  curl -fLo $HOME/.vim/colors/solarized.vim --create-dirs \
-  https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
-fi
-
-# Add palenight colors for vim if not present
-if [ ! -f $HOME/.vim/colors/palenight.vim ]; then
-  curl -fLo $HOME/.vim/colors/palenight.vim --create-dirs \
-  https://raw.githubusercontent.com/drewtempelmeyer/palenight.vim/master/colors/palenight.vim
-fi
-
-# Add lucius colors for vim if not present
-if [ ! -f $HOME/.vim/colors/lucius.vim ]; then
-  curl -fLo $HOME/.vim/colors/lucius.vim --create-dirs \
-  https://raw.githubusercontent.com/maksimr/Lucius2/master/colors/lucius.vim
-fi
+ln $params $SCRIPT_HOME/custom.bash $HOME/.bash_it/lib/custom.bash
 
 # Symlink all of our dotfiles to the home directory
 for i in .vimrc .dircolors .bashrc .bash_profile .bash_darwin .tmux.conf .gdbinit .gitconfig;
 do
-  ln $params $script_home/$i $HOME/$i
+  [ -e $HOME/$i ] && mv $HOME/$i $HOME/$i.backup
+  ln $params $SCRIPT_HOME/$i $HOME/$i
 done
